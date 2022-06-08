b248894a3e2283bc64057ab77e2a9ea903b03a9f
Ramil Flores <ramil.flores@ext-rangeforce.com>
2022-06-08 08:48:19 +0000

    Add elastic-agent config for testing.

---
+++ enable-elastic-agent.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-7.17.4-windows-x86_64.zip -O ansible/roles/windows/elastic-agent/files/elastic-agent-7.17.4-windows-x86_64.zip
+unzip ansible/roles/windows/elastic-agent/files/elastic-agent-7.17.4-windows-x86_64.zip -d ansible/roles/windows/elastic-agent/files/
+
+cd ansible
+ansible-playbook -i inventory playbook-elastic-agent.yml
+
+echo ""
+echo ""
+echo "Action complete."
+echo ""
+cd ..
