70dc54a520a3dd65c0fdf4057e339c9c7e7c751f
Ramil Flores <ramil.flores@ext-rangeforce.com>
2022-06-08 09:17:01 +0000

    Add elastic-agent config for testing.

---
+++ ansible/roles/windows/elastic-agent/files/install.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\elastic-agent\elastic-agent-7.17.4-windows-x86_64\elastic-agent.exe install -f --url=https://172.17.60.50:8220 --enrollment-token=SEtOVHYzOEIwYzJrYk01WUJqYVk6a3NoMWJVQkdUMlcwU3RQRl9yYW5qdw=="
