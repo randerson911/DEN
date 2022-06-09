3627d33bd204c86d8d6a33f3aafcbfeff2f9ef1a
Ramil Flores <ramil.flores@ext-rangeforce.com>
2022-06-09 15:23:44 +0000

    Added GNS3 auto-tap script.

---
+++ tap-collector.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo ""
+echo "Important: If you encounter errors in the ansible output, it will most likely be"
+echo "credentials related. Please ensure you are providing the Linux credentials in"
+echo "the following prompts."
+echo ""
+
+cd ansible
+ansible-collection collection install davidban77.gns3
+ansible-playbook -i inventory playbook-tap-collector.yml -K
+echo ""
+echo ""
+echo "Action complete."
+echo ""
+cd ..
