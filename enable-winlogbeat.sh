1c4bba72731c47a99e767651721ddd2552acbab7
Ramil Flores <ramil.flores@ext-rangeforce.com>
2022-07-27 20:14:25 +0000

    enable timefield in winlogbeat script.

--- enable-winlogbeat.sh
+++ enable-winlogbeat.sh
@@ -46,7 +46,8 @@ creds=$(cat elmarker.txt)
 curl -u $creds -k -X POST "https://172.17.60.40:5601/api/index_patterns/index_pattern" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d'
 {
   "index_pattern": {
-     "title": "winlogbeat-*"
+     "title": "winlogbeat-*",
+     "timeFieldName": "@timestamp"
   }
 }
 '
