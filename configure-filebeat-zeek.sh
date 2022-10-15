01503c7010dd3a8b9d95ddac6b648438c53dd3ad
shift-f10 <40450442+shift-f10@users.noreply.github.com>
2022-10-14 21:19:17 -0700

    added filebeat zeek configuration script

---
+++ configure-filebeat-zeek.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ ! -f linuxcreds.txt ]
+then
+    echo ""
+    echo "Important: If you encounter errors in the ansible output, it will most likely be"
+    echo "credentials related. Please ensure you are providing the Kali Red credentials in"
+    echo "the following prompts. Delete the linuxcreds.txt and input the correct values."
+    echo ""
+
+    echo "Enter linux username: "
+    read linuxuser
+
+    echo "Enter linux password: "
+    read linuxpass
+
+    sed -i "/linux_user:/c\linux_user: $linuxuser" ansible/group_vars/all/vars.yml
+    sed -i "/linux_user_password:/c\linux_user_password: $linuxpass" ansible/group_vars/all/vars.yml
+    echo "Complete." > linuxcreds.txt
+fi
+
+cd ansible
+ansible-playbook -i inventory playbook.yml
+
+cd ..
+
+echo ""
+echo ""
+echo "Action complete."
+echo ""
