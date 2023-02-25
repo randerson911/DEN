56c56b4a0d58e4263893539669373c9533579929
randerson911 <github@rahoemail.us>
2023-02-25 14:13:33 -0600

    Fixed the missing Username issue for Kickoff

--- kickoff.sh
+++ kickoff.sh
@@ -1,59 +1,5 @@
 #! /bin/bash
 
-## There is a large comment block here, as capabilities have moved to an encrypted ansible vault.
-
-# if [ ! -f rmarker.txt ]
-# then
-#     echo ""
-#     echo "Important: If you encounter errors in the ansible output, it will most likely be"
-#     echo "credentials related. Please ensure you are providing the Kali Red credentials in"
-#     echo "the following prompts. Delete the rmarker.txt and input the correct values."
-#     echo ""
-
-#     echo "Enter Kali username: "
-#     read reduser
-
-#     echo "Enter Kali password: "
-#     read redpass
-
-#     sed -i "/red_user:/c\red_user: $reduser" ansible/group_vars/all/vars.yml
-#     sed -i "/red_user_password:/c\red_user_password: $redpass" ansible/group_vars/all/vars.yml
-#     echo "Complete." > rmarker.txt
-# fi
-
-# if [ ! -f umarker.txt ]
-# then
-#     echo ""
-#     echo "Important: If you encounter errors in the ansible output, it will most likely be"
-#     echo "credentials related. Please ensure you are providing the Windows credentials in"
-#     echo "the following prompts. Delete the umarker.txt and input the correct values."
-#     echo ""
-
-#     echo "Enter Windows admin username: "
-#     read denuser
-
-#     echo "Enter Windows admin password: "
-#     read denpass
-
-#     sed -i "/den_user:/c\den_user: $denuser" ansible/group_vars/all/vars.yml
-#     sed -i "/den_user_password:/c\den_user_password: $denpass" ansible/group_vars/all/vars.yml
-#     echo "Complete." > umarker.txt
-# fi
-
-# lpass=""
-# if [ ! -f linuxcreds.txt ]
-# then
-#     echo "What is the linux password?"
-#     read lpass
-#     sed -i "/linux_user:/c\linux_user: cobra" ansible/group_vars/all/vars.yml
-#     sed -i "/linux_user_password:/c\linux_user_password: $lpass" ansible/group_vars/all/vars.yml
-#     echo "Complete." > linuxcreds.txt
-# else 
-#     lpass=$(grep "linux_user_password:" ./ansible/group_vars/all/vars.yml | awk '{print $2}')
-# fi
-# echo $lpass | sudo -S apt update
-# echo $lpass | sudo -S apt install unzip -y
-
 if [ ! -f ansible/.vault_pass ]
 then
     echo "Please enter the vault password: "
@@ -61,6 +7,58 @@ then
 
     echo $vault_pass > ./ansible/.vault_pass
     chmod 0600 ./ansible/.vault_pass
+fi
+
+if [ -f users.txt ]
+then
+    echo "It looks like you already have a list of users."
+    echo "Do you wish to generate a new one?"
+    echo ""
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) rm -f users.txt; break;;
+            No ) break;;
+        esac
+    done
+fi
+
+if [ ! -f users.txt ]
+then
+    cd ansible
+    ansible-playbook -i inventory --vault-password-file ./.vault_pass playbook-get-random-users.yml
+    cd ..
+    sed -i 's/[^[:print:]]//g' users.txt
+    sed -i '/^$/d' users.txt
+    sed -i '1d' users.txt
+    sed -r -i 's/\s+//g' users.txt
+    cat users.txt
+fi
+
+if [ ! -f lmarker.txt ]
+then
+
+    if grep -q "user1:vars" ansible/inventory
+    then
+        lnr=$(sed -n '/##marker/=' ansible/inventory)
+        sed -i "1, $lnr ! d" ansible/inventory
+    fi
+
+    echo ""
+    echo "Modify inventory file and integrate selected users."
+
+    c=0
+    var1="user"
+    var2=":vars"
+    varname="uname="
+
+    cat users.txt | while read line
+    do
+        let c=c+1
+        echo "" >> ansible/inventory
+        echo [$var1$c$var2] >> ansible/inventory
+        echo $varname$line >> ansible/inventory
+    done
+    echo "Complete." > lmarker.txt
 fi
 
 if [ ! -f elmarker.txt ]
