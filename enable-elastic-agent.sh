d959583e6498a4080e3ad3a1f4514624b596d66e
randerson911 <github@rahoemail.us>
2023-02-26 22:41:07 +0000

    Feb 26, 2023, 4:41 PM

--- enable-elastic-agent.sh
+++ enable-elastic-agent.sh
@@ -22,6 +22,12 @@ if [ ! -f ansible/roles/windows/elastic-agent/files/elastic-agent-7.17.4-windows
     unzip ansible/roles/windows/elastic-agent/files/elastic-agent-7.17.4-windows-x86_64.zip -d ansible/roles/windows/elastic-agent/files/
 fi
 
+if [ ! -f ansible/roles/linux/elastic-agent/files/elastic-agent-7.17.4-linux-x86_64.tar.gz ]; then
+    wget https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-7.17.4-linux-x86_64.tar.gz -O ansible/roles/linux/elastic-agent/files/elastic-agent-7.17.4-linux-x86_64.tar.gz
+    tar -xzvf ansible/roles/linux/elastic-agent/files/elastic-agent-7.17.4-linux-x86_64.tar.gz -C ansible/roles/linux/elastic-agent/files/
+fi
+
+
 cd ansible
 ansible-playbook -i inventory.yml --vault-password-file ./.vault_pass -e "elastic_targets=datacenter,subnet1,subnet2,subnet3" playbook-elastic-agent.yml
 
