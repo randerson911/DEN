2ff516efa5a0798abf99543592ad442655441197
randerson911 <github@rahoemail.us>
2023-02-26 08:42:09 -0600

    Pip update

--- update-ansible.sh
+++ update-ansible.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
 # Check installed Ansible version
-installed_version=$(ansible --version | grep "^ansible" | awk '{print $2}')
+pip install --upgrade --user $(pip freeze | awk '{split($0, a, "=="); print a[1]}')
 
-# Check latest Ansible version
-latest_version=$(curl -s https://api.github.com/repos/ansible/ansible/releases/latest | grep tag_name | cut -d '"' -f 4)
-
-# Compare versions and upgrade if necessary
-if [[ "$installed_version" != "$latest_version" ]]; then
-    echo "Upgrading Ansible from $installed_version to $latest_version"
-    pip install ansible=="$latest_version"
-else
-    echo "Ansible is already up to date"
-fi
