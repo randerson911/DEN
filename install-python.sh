43a2e8732b8a0f99a6d0b1ed3ee6adf2ff7ac90a
randerson911 <github@rahoemail.us>
2023-02-27 22:00:14 +0000

    Feb 27, 2023, 4:00 PM

--- install-python.sh
+++ install-python.sh
@@ -10,15 +10,15 @@ then
     chmod 0600 ./ansible/.vault_pass
 fi
 
-# Download and move latest version of Python for Windows
-if [ ! -f ansible/roles/windows/install-python311/files/python311.exe ]; then
-  curl -o ansible/roles/windows/install-python311/files/python311.exe https://www.python.org/ftp/python/3.11.2/python-3.11.2-amd64.exe
-fi
+# # Download and move latest version of Python for Windows
+# if [ ! -f ansible/roles/windows/install-python311/files/python311.exe ]; then
+#   curl -o ansible/roles/windows/install-python311/files/python311.exe https://www.python.org/ftp/python/3.11.2/python-3.11.2-amd64.exe
+# fi
 
-# Download and move latest version of Python for Linux
-if [ ! -f ansible/roles/linux/install-python311/files/python311.tar.gz ]; then
-  curl -o ansible/roles/linux/install-python311/files/python311.tar.gz https://www.python.org/ftp/python/3.11.2/Python-3.11.2.tar.xz
-fi
+# # Download and move latest version of Python for Linux
+# if [ ! -f ansible/roles/linux/install-python311/files/python311.tar.gz ]; then
+#   curl -o ansible/roles/linux/install-python311/files/python311.tar.gz https://www.python.org/ftp/python/3.11.2/Python-3.11.2.tar.xz
+# fi
 
 ansible-galaxy collection install ansible.windows
 ansible-galaxy collection install ansible.posix
