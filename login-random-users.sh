02382d10b82f14c9133331a967d09d15d43890d4
randerson911 <github@rahoemail.us>
2023-02-27 02:34:46 +0000

    Feb 26, 2023, 8:34 PM

--- login-random-users.sh
+++ login-random-users.sh
@@ -41,6 +41,9 @@ if [ ! -f lmarker.txt ]; then
   lpass=$(ansible-vault view --vault-password-file ./.vault_pass cobra.vault | grep linux_user_password | cut -d ' ' -f 2)
   cd ..
 
+  echo " Installing the required application to modify the yaml file"
+  echo ""
+  echo ""
   echo $lpass | sudo -S apt update
   echo $lpass | sudo -S apt install yq -y
 
