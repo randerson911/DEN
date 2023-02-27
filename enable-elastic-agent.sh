5edc7189a65a4cbffe0e044e376c2def59bfe003
randerson911 <github@rahoemail.us>
2023-02-27 22:29:30 +0000

    Feb 27, 2023, 4:29 PM

--- enable-elastic-agent.sh
+++ enable-elastic-agent.sh
@@ -14,18 +14,18 @@ cd ansible
 lpass=$(ansible-vault view --vault-password-file ./.vault_pass cobra.vault | grep linux_user_password | cut -d ' ' -f 2)
 cd ..
 
-echo $lpass | sudo -S apt update
-echo $lpass | sudo -S apt install unzip -y
+# echo $lpass | sudo -S apt update
+# echo $lpass | sudo -S apt install unzip -y
 
-if [ ! -f ansible/roles/windows/elastic-agent/files/elastic-agent-7.17.4-windows-x86_64.zip ]; then
-    wget https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-7.17.4-windows-x86_64.zip -O ansible/roles/windows/elastic-agent/files/elastic-agent-7.17.4-windows-x86_64.zip
-    unzip ansible/roles/windows/elastic-agent/files/elastic-agent-7.17.4-windows-x86_64.zip -d ansible/roles/windows/elastic-agent/files/
-fi
+# if [ ! -f ansible/roles/windows/elastic-agent/files/elastic-agent-7.17.4-windows-x86_64.zip ]; then
+#     wget https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-7.17.4-windows-x86_64.zip -O ansible/roles/windows/elastic-agent/files/elastic-agent-7.17.4-windows-x86_64.zip
+#     unzip ansible/roles/windows/elastic-agent/files/elastic-agent-7.17.4-windows-x86_64.zip -d ansible/roles/windows/elastic-agent/files/
+# fi
 
-if [ ! -f ansible/roles/linux/elastic-agent/files/elastic-agent-7.17.4-linux-x86_64.tar.gz ]; then
-    wget https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-7.17.4-linux-x86_64.tar.gz -O ansible/roles/linux/elastic-agent/files/elastic-agent-7.17.4-linux-x86_64.tar.gz
-    tar -xzvf ansible/roles/linux/elastic-agent/files/elastic-agent-7.17.4-linux-x86_64.tar.gz -C ansible/roles/linux/elastic-agent/files/
-fi
+# if [ ! -f ansible/roles/linux/elastic-agent/files/elastic-agent-7.17.4-linux-x86_64.tar.gz ]; then
+#     wget https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-7.17.4-linux-x86_64.tar.gz -O ansible/roles/linux/elastic-agent/files/elastic-agent-7.17.4-linux-x86_64.tar.gz
+#     tar -xzvf ansible/roles/linux/elastic-agent/files/elastic-agent-7.17.4-linux-x86_64.tar.gz -C ansible/roles/linux/elastic-agent/files/
+# fi
 
 
 cd ansible
