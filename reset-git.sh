9b2057418bf4acb7926e18a21e1b00bef3190160
randerson911 <github@rahoemail.us>
2023-11-11 10:34:15 -1000

    Rework Started

--- reset-git.sh
+++
@@ -1,5 +0,0 @@
-rm ./umarket.txt 2>/dev/null
-rm ./users.txt 2>/dev/null
-rm ./linux.txt 2>/dev/null
-rm ./lmarker.txt 2>/dev/null
-rm ./cobra.vault 2>/dev/null
--- update-ansible.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Check installed Ansible version
-pip install --upgrade --user $(pip freeze | awk '{split($0, a, "=="); print a[1]}')
-
