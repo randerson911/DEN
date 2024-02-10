a568006627c95496a6d8d16a07a9a2f96a2dd954
randerson911 <github@rahoemail.us>
2024-02-10 05:52:24 +0000

    Feb 9, 2024, 7:52 PM

---
+++ .venv/lib/python3.11/site-packages/construct/version.py
@@ -0,0 +1,3 @@
+version = (2,10,69)
+version_string = "2.10.69"
+release_date = "2023.10.22"
---
+++ run_scripts/playbookSetupRange.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+ansible-playbook --ask-for-vault-password ../playbook/playbookSetupRange.yml
